acca28fcadd4c2f9d85493ed931e8a4d399ebc05
CharlieXiao <32426723+CharlieXiao@users.noreply.github.com>
2021-01-03 23:32:45 +0800

    添加异常处理和返回函数

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
+def success(data):
+    catch_data = {
+        "code": 0,
+        "msg": "",
+        "data": data
+    }
+    return jsonify(catch_data)
+
+
 @app.route('/')
 def hello_world():
-    dic = {"name":"Bob","properties":{"age":"18","gender":"male"}}
-    return dic
+    dic = {"name": "Bob", "properties": {"age": "18", "gender": "male"}}
+    return success(dic)
+
+
+@app.errorhandler(Exception)
+def error_handler(e):
+    """
+    全局异常捕获
+    """
+    catch_data = {
+        "code": -1,
+        "msg": repr(e),
+        "data": None
+    }
+    # print(repr(e))
+    # print(str(e))
+    # 异常追踪
+    # traceback.print_exc()
+    return jsonify(catch_data)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5000, host="127.0.0.1", debug=True)
