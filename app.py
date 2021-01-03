1a5e3eae229c1278d9239886a787a1834178c187
CharlieXiao <32426723+CharlieXiao@users.noreply.github.com>
2021-01-04 00:07:20 +0800

    添加numpy存储，用于存储稀疏矩阵数据

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 import pymongo
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 # 连接数据库
@@ -26,6 +28,22 @@ def hello_world():
     # bson.decode()
     # dic = {"name": "Bob", "properties": {"age": "18", "gender": "male"}}
     return success(test_data)
+
+
+"""
+# serialize 1D array x
+record['feature1'] = x.tolist()
+
+# deserialize 1D array x
+x = np.fromiter( record['feature1'] )
+对于多维数组，你需要使用pickle和pymongo.binary.Binary：
+
+# serialize 2D array y
+record['feature2'] = pymongo.binary.Binary( pickle.dumps( y, protocol=2) ) )
+
+# deserialize 2D array y
+y = pickle.loads( record['feature2'] )
+"""
 
 
 @app.errorhandler(Exception)
